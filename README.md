91a4149652f7fe2ff3921971528ac0d4283d052b
andrey kliemnko <andrey.klimenko@verivox.com>
2022-04-01 21:17:44 +0200

    Removed junk

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # login-test
 
 We assume that this service is micro
-Using .net core Identity is an overkill 
-We assume that the cookie consent was given if we plan to use cookies
+
+The script to create the DB is in Data/SqlScripts
--- UserLoginFormTest.IntegrationTests/GlobalUsings.cs
+++ UserLoginFormTest.IntegrationTests/GlobalUsings.cs
@@ -1,7 +1,5 @@
 global using FluentAssertions;
 global using NUnit.Framework;
-global using System;
-global using System.Collections.Generic;
 global using System.Threading.Tasks;
 global using UserLoginFormTest.Data;
 global using UserLoginFormTest.Data.Commands;
